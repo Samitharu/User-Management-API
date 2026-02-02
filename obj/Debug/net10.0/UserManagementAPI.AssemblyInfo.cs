643b19e55dcf38b0f2d63126c1ab3ac9e2594b57
Samitha Lowe <58819808+Samitharu@users.noreply.github.com>
2026-02-02 22:40:35 +0530

    file upload after runing the project

--- obj/Debug/net10.0/UserManagementAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/UserManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4303bbd1326a07a5f4a3b722a2fb3188d5684b4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
